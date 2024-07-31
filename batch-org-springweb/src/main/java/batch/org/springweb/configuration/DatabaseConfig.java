cb7956280887503d02aec17ec735f646f579100d
sirajprojects <87057875+sirajprojects@users.noreply.github.com>
2024-08-01 00:58:18 +0530

    add

--- batch-org-springweb/src/main/java/batch/org/springweb/configuration/DatabaseConfig.java
+++ batch-org-springweb/src/main/java/batch/org/springweb/configuration/DatabaseConfig.java
@@ -1,23 +1,22 @@
-//package batch.org.springweb.configuration;
-//
-//
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//
-//@Configuration
-//public class DatabaseConfig {
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSource.setUrl("jdbc:mysql://localhost:3306/batch");
-//        dataSource.setUsername("root");
-//        dataSource.setPassword("root");
-//        return dataSource;
-//    }
-//}
+package batch.org.springweb.configuration;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/batch");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+}
--- batch-org-springweb/src/main/java/batch/org/springweb/dao/QueryRepositoryImpl.java
+++ batch-org-springweb/src/main/java/batch/org/springweb/dao/QueryRepositoryImpl.java
@@ -1,22 +1,51 @@
+
 package batch.org.springweb.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class QueryRepositoryImpl implements QueryRepository {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
     @Override
     public List<Map<String, Object>> executeQuery(String query, Object... params) {
         System.out.println("Executing query: " + query);
-        List<Map<String, Object>> results = jdbcTemplate.queryForList(query, params);
+        List<Map<String, Object>> results = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            for (int i = 0; i < params.length; i++) {
+                ps.setObject(i + 1, params[i]);
+            }
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Map<String, Object> row = new HashMap<>();
+                    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                        row.put(rs.getMetaData().getColumnName(i), rs.getObject(i));
+                    }
+                    results.add(row);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Query executed. Number of records: " + results.size());
         return results;
     }
